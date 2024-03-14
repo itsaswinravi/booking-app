df4734eda4b730a7a771dae69284e85f008ff5a0
itsaswinravi <achu322001@gmail.com>
2024-03-14 13:20:26 +0530

    2.40

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { Route, Routes } from 'react-router-dom';
 import IndexPage from './pages/IndexPage.jsx';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import PlacesPage from './pages/PlacesPage';
 import Layout from './Layout';
 import axios from 'axios';
 import { UserContextProvider } from './UserContext';
@@ -26,6 +27,7 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/account/:subpage?" element={<AccountPage />}/>
+      <Route path="/account/:subpage/:action" element={<AccountPage />}/>
       
       </Route>
       
