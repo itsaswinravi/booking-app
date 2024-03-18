7a2d245e44124efeb8c938326c594c2973793764
itsaswinravi <achu322001@gmail.com>
2024-03-18 17:31:55 +0530

    5.15

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -14,8 +14,28 @@ export default function PlacesPage(){
     },[id]);
     if(!place) return '';
     return(
-        <div className="mt-8">
-            <h1 className="text-2xl">{place.title}</h1>
+        <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+            <h1 className="text-3xl">{place.title}</h1>
+            <a className=" my-2 block font-semibold underline" target="_blank" href={'https://maps.google.com/?q'+place.address}>{place.address}</a>
+             <div className="grid gap-2 grid-cols-[2fr_1fr]">
+                <div>
+                    {place.photos?.[0] && (
+                        <img src={'http://localhost:4000/uploads/'+place.photos?.[0]} alt="" />
+
+                    )}
+                </div>
+
+                <div>
+                {place.photos?.[1] && (
+                        <img src={'http://localhost:4000/uploads/'+place.photos?.[1]} alt="" />
+
+                    )}
+                    {place.photos?.[2] && (
+                        <img src={'http://localhost:4000/uploads/'+place.photos?.[2]} alt="" />
+
+                    )}
+                </div>
+             </div>
         </div>
     );
 }
